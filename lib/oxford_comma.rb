7054ceff0f85c1608c84e0f76d6756033e98bc04
Keanna Salter <keannasalter91@gmail.com>
2020-01-30 21:57:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ if array.size == 1
     array.join
   elsif array.size == 2
     array.join(" and ")
-  else array.size >= 3
-    array.last.insert(0," and ")
+  else array.size == 3
+    array[2] = "and starfruit"
     array.join(",")
 end
 end
